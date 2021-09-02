33d4b8edc408efa23627fc40387c404ef4fa01d9
ZmnSCPxj jxPCSnmZ <ZmnSCPxj@protonmail.com>
2021-09-02 23:46:55 +0800

    common/features.c: Add from lightningd.

---
+++ common/memleak.h
@@ -0,0 +1,9 @@
+#ifndef PAYZ_COMMON_MEMLEAK_H
+#define PAYZ_COMMON_MEMLEAK_H
+
+/* This file is included by common/features.c, but
+ * that module does not seem to actually refer to
+ * anything in this file.
+ */
+
+#endif /* PAYZ_COMMON_MEMLEAK_H */
---
+++ wire/peer_wire.h
@@ -0,0 +1,12 @@
+#ifndef PAYZ_WIRE_PEER_WIRE_H
+#define PAYZ_WIRE_PEER_WIRE_H
+#include<wire/wire.h>
+
+/* Shim.
+ *
+ * common/features.c includes this file, but seems to just
+ * use plain wire/wire.h interfaces.
+ * So this include file shim just includes that file.
+ */
+
+#endif /* PAYZ_WIRE_PEER_WIRE_H */
