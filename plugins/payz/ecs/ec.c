d09a15a7eee61b86161d210ad8f925f2718e6223
ZmnSCPxj jxPCSnmZ <ZmnSCPxj@protonmail.com>
2021-09-05 18:38:31 +0800

    plugins/payz/ecs/ec.c: Start module to handle entity-component table.

---
+++ plugins/payz/ecs/ec.c
@@ -0,0 +1,306 @@
+#include"ec.h"
+#include<assert.h>
+#include<ccan/intmap/intmap.h>
+#include<ccan/strmap/strmap.h>
+#include<ccan/tal/str/str.h>
+#include<common/json.h>
+#include<common/utils.h>
+#include<string.h>
+
+/*~
+ * The EC object is a mapping of entities to
+ * struct ec_entityrow.
+ * A struct ec_entityrow is a mapping of
+ * component names to struct ec_cell.
+ * An ec_cell is really just a string buffer
+ * and pre-parsed JSON token.
+ */
+
+/** struct ec_cell
+ *
+ * @brief Represents a JSON datum of a component,
+ */
+struct ec_cell {
+	char *buffer;
+	jsmntok_t *tok;
+};
+
+/** struct ec_entityrow
+ *
+ * @brief Represents the components attached to an
+ * entity.
+ */
+struct ec_entityrow {
+	/** Mapping from component name to cell.  */
+	STRMAP(struct ec_cell *) component_map;
+};
+
+struct ec {
+	/** The next entity available to allocate.  */
+	u32 next_entity;
+
+	/** Tal-allocated "null" JSON datum, used to return an
+	 * ec_getcomponent call if the requested component
+	 * is not attached.
+	 */
+	char *null_buffer;
+	jsmntok_t *null_tok;
+
+	/** Mapping from entity ID to entity row.  */
+	UINTMAP(struct ec_entityrow *) entity_map;
+};
+
+static void destroy_ecs(struct ec *ec);
+static void destroy_entityrow(struct ec_entityrow *entityrow);
+
+struct ec *ec_new(const tal_t *ctx)
+{
+	struct ec *ec = tal(ctx, struct ec);
+
+	ec->next_entity = 1;
+	ec->null_buffer = tal_strdup(ec, "null");
+	ec->null_tok = tal_arr(ec, jsmntok_t, 1);
+	ec->null_tok[0].type = JSMN_PRIMITIVE;
+	ec->null_tok[0].start = 0;
+	ec->null_tok[0].end = 4;
+	ec->null_tok[0].size = 0;
+	uintmap_init(&ec->entity_map);
+
+	tal_add_destructor(ec, &destroy_ecs);
+
+	return ec;
+}
+
+u32 ec_newentity(struct ec *ec)
+{
+	return ec->next_entity++;
+}
+
+void ec_get_entity_bounds(const struct ec *ec,
+			   u32 *min,
+			   u32 *max)
+{
+	intmap_index_t im_min;
+	intmap_index_t im_max;
+	if (uintmap_empty(&ec->entity_map)) {
+		*min = 0;
+		*max = 0;
+		return;
+	}
+
+	(void) uintmap_first(&ec->entity_map, &im_min);
+	(void) uintmap_last(&ec->entity_map, &im_max);
+
+	*min = (u32) im_min;
+	*max = (u32) im_max + 1;
+}
+
+static bool ec_get_components_from_strmap(const char *name,
+					   struct ec_cell *value,
+					   char ***components);
+char **ec_get_components(const tal_t *ctx,
+			  const struct ec *ec,
+			  u32 entity)
+{
+	char **components = NULL;
+	struct ec_entityrow *entityrow = uintmap_get(&ec->entity_map,
+						      entity);
+
+	if (!entityrow)
+		return NULL;
+
+	components = tal_arr(ctx, char*, 0);
+	strmap_iterate(&entityrow->component_map,
+		       &ec_get_components_from_strmap, &components);
+	/* If the components were empty then the entityrow should have
+	 * been deleted.  */
+	assert(tal_count(components) != 0);
+
+	return components;
+}
+static bool ec_get_components_from_strmap(const char *name,
+					   struct ec_cell *value,
+					   char ***components)
+{
+	tal_arr_expand(components, tal_strdup(*components, name));
+	return true;
+}
+
+bool ec_get_component(const struct ec *ec,
+		       const char **buffer,
+		       const jsmntok_t **toks,
+		       u32 entity,
+		       const char *component)
+{
+	struct ec_entityrow *entityrow;
+	struct ec_cell *cell;
+
+	entityrow = uintmap_get(&ec->entity_map, entity);
+	if (!entityrow) {
+		*buffer = ec->null_buffer;
+		*toks = ec->null_tok;
+		return false;
+	}
+
+	cell = strmap_get(&entityrow->component_map, component);
+	if (!cell) {
+		*buffer = ec->null_buffer;
+		*toks = ec->null_tok;
+		return false;
+	}
+
+	*buffer = cell->buffer;
+	*toks = cell->tok;
+	return true;
+}
+
+static struct ec_entityrow *ec_entityrow_new(const tal_t *ctx);
+static struct ec_cell *ec_cell_new(const tal_t *ctx,
+				   const char *buffer,
+				   const jsmntok_t *tok);
+void ec_set_component(struct ec *ec,
+		       u32 entity,
+		       const char *component,
+		       const char *buffer,
+		       const jsmntok_t *tok)
+{
+	bool detach = false;
+	struct ec_entityrow *entityrow;
+	struct ec_cell *cell;
+
+	if (!buffer || !tok) {
+		assert(!buffer && !tok);
+		detach = true;
+	}
+
+	if (tok->type == JSMN_PRIMITIVE && buffer[tok->start] == 'n')
+		detach = true;
+
+	entityrow = uintmap_get(&ec->entity_map, entity);
+
+	if (detach) {
+		/* Nothing to detach?  */
+		if (!entityrow)
+			return;
+		cell = strmap_get(&entityrow->component_map, component);
+		if (!cell)
+			return;
+
+		strmap_del(&entityrow->component_map, component, NULL);
+		tal_free(cell);
+
+		/* If entity row is now empty, also delete the row.  */
+		if (strmap_empty(&entityrow->component_map)) {
+			uintmap_del(&ec->entity_map, entity);
+			tal_free(entityrow);
+		}
+	} else {
+		/* No components yet?  */
+		if (!entityrow) {
+			entityrow = ec_entityrow_new(ec);
+			uintmap_add(&ec->entity_map, entity, entityrow);
+			cell = NULL;
+		} else
+			cell = strmap_get(&entityrow->component_map, component);
+
+		/* Detach first if already exist.  */
+		if (cell) {
+			strmap_del(&entityrow->component_map, component, NULL);
+			cell = tal_free(cell);
+		}
+
+		/* Now attach.  */
+		cell = ec_cell_new(entityrow, buffer, tok);
+		strmap_add(&entityrow->component_map, component, cell);
+	}
+}
+
+static struct ec_entityrow *ec_entityrow_new(const tal_t *ctx)
+{
+	struct ec_entityrow *entityrow = tal(ctx, struct ec_entityrow);
+
+	strmap_init(&entityrow->component_map);
+
+	tal_add_destructor(entityrow, &destroy_entityrow);
+
+	return entityrow;
+}
+
+static const jsmntok_t *json_tok_end(const jsmntok_t *tok);
+static struct ec_cell *ec_cell_new(const tal_t *ctx,
+				   const char *buffer,
+				   const jsmntok_t *tok)
+{
+	struct ec_cell *cell = tal(ctx, struct ec_cell);
+
+	/* Determine text buffer size to copy.  */
+	const char *to_copy = json_tok_full(buffer, tok);
+	int len = json_tok_full_len(tok);
+	/* Determine token array size to copy.  */
+	const jsmntok_t *tok_end = json_tok_end(tok);
+
+	/* The offset to apply to all copied tokens.  */
+	int offset = to_copy - buffer;
+
+	/* Load the cell.  */
+	cell->buffer = tal_dup_arr(cell, char,
+				   to_copy, len, 0);
+	cell->tok = tal_dup_arr(cell, jsmntok_t,
+				tok, tok_end - tok, 0);
+	/* Adjust the copied tokens by the offset.  */
+	if (offset != 0) {
+		int i;
+		for (i = 0; i < tal_count(cell->tok); ++i) {
+			cell->tok[i].start -= offset;
+			cell->tok[i].end -= offset;
+		}
+	}
+
+	return cell;
+}
+static const jsmntok_t *json_tok_end(const jsmntok_t *tok)
+{
+	int size = tok->size;
+	++tok;
+	for (; size != 0; --size)
+		tok = json_tok_end(tok);
+	return tok;
+}
+
+void ec_set_component_datum(struct ec *ec,
+			    u32 entity,
+			    const char *component,
+			    const char *valuez)
+{
+	ec_set_component(ec, entity, component, valuez,
+			 json_parse_simple(tmpctx, valuez, strlen(valuez)));
+}
+
+/*-----------------------------------------------------------------------------
+EC Destructor
+-----------------------------------------------------------------------------*/
+/*~
+ * The ccan intmap and strmap modules do not use tal, but instead
+ * use malloc.
+ *
+ * To ensure that malloc-allocated objects get freed when the EC
+ * object itself is freed, we need to clear each entity row
+ * strmap, then clear the entity intmap.
+ */
+
+static void destroy_entityrow(struct ec_entityrow *entityrow)
+{
+	/* Cells are tal-allocated, so no need to delete the
+	 * contained cells.  */
+
+	strmap_clear(&entityrow->component_map);
+}
+
+static void destroy_ecs(struct ec *ec)
+{
+	/* Entity rows are tal-allocated, so no need to delete
+	 * the contained rows.
+	 */
+
+	uintmap_clear(&ec->entity_map);
+}
---
+++ plugins/payz/ecs/ec.h
@@ -0,0 +1,162 @@
+#ifndef LIGHTNING_PLUGINS_PAYZ_ECS_EC_H
+#define LIGHTNING_PLUGINS_PAYZ_ECS_EC_H
+#include"config.h"
+#include<ccan/short_types/short_types.h>
+#include<ccan/tal/tal.h>
+#include<external/jsmn/jsmn.h>
+#include<stdbool.h>
+#include<stdlib.h>
+
+/** struct ec
+ *
+ * @brief Represents an instance of an Entity-Component table,
+ * which attaches named components to numeric ID entities.
+ *
+ * @desc This implements the data of an ECS framework object.
+ */
+struct ec;
+
+/** ec_new
+ *
+ * @brief Constructs an instance of the Entity-Component table.
+ *
+ * @param ctx - The owner of this EC table.
+ */
+struct ec *ec_new(const tal_t *ctx);
+
+/** ec_newentity
+ *
+ * @brief Allocate a fresh entity ID number.
+ *
+ * @param ec - The EC instance to allocaate from.
+ *
+ * @return - An entity ID that no previous ec_newentity on
+ * this instance has returned.
+ */
+u32 ec_newentity(struct ec *ec);
+
+/** ec_get_entity_bounds
+ *
+ * @brief Return the minimum and maximum bounds of entities
+ * in this EC instance.
+ * The minimum bound is inclusive, the maximum bound is
+ * exclusive, i.e. min <= entity < max.
+ *
+ * @desc This provides bounds that may be looser than actual.
+ * It will return min == max if there are no entities with
+ * any attached components.
+ *
+ * @param ec - the EC instance to query.
+ * @param min - output, the minimum entity.
+ * @param max - output, one past the maximum entity.
+ */
+void ec_get_entity_bounds(const struct ec *ec,
+			   u32 *min,
+			   u32 *max);
+
+/** ec_get_components
+ *
+ * @brief Gets the component names of components attached to
+ * the given entity.
+ *
+ * @param ctx - the tal context to allocate the returned
+ * array from.
+ * @param ec - the EC instance to query.
+ * @param entity - the entity whose attached components will
+ * be returned.
+ *
+ * @return - a tal-allocated array of null-terminated strings
+ * naming the components attached to this entity.
+ * The array has the given ctx as parent, while each string
+ * has the array as parent.
+ * Return NULL if the entity has no attached components.
+ * The returned array is sorted by a trivial byte-based
+ * compare (i.e. strcmp).
+ */
+char **ec_get_components(const tal_t *ctx,
+			  const struct ec *ec,
+			  u32 entity);
+
+/** ec_get_component
+ *
+ * @brief Gets the value of the given component attached to the
+ * given entity.
+ *
+ * @param ec - the EC instance to query.
+ * @param buffer - output, the tal-allocated string buffer
+ * containing the raw JSON text.
+ * The ec instance owns the storage for this string buffer,
+ * and the storage may be invalidated if you run
+ * ec_set_component afterwards.
+ * The buffer is *not* null-terminated; use the toks->end
+ * below to determine the usable extent of the buffer.
+ * @param toks - output, the tal-allocated array of tokens
+ * representing the JSON value.
+ * The ec instance owns the storage for this token array,
+ * and the storage may be invalidated if you run
+ * ec_set_component afterwards.
+ * @param entity - the numeric ID of the entity to look up.
+ * @param component - the name of the component to look up.
+ *
+ * @return - true if the entity has the component attached,
+ * false if the entity does not have the component attached.
+ * Note that if this returns false, buffer and toks are still
+ * set to a "null" JSON datum.
+ */
+bool ec_get_component(const struct ec *ec,
+		       const char **buffer,
+		       const jsmntok_t **toks,
+		       u32 entity,
+		       const char *component);
+
+/** ec_set_component
+ *
+ * @brief Attaches, detaches, or mutates the component
+ * attached to the given entity.
+ *
+ * @desc If the given value is a JSON null or a C NULL,
+ * then the component is detached if it is currently
+ * attached (no change if not currently attached).
+ * Otherwise, it attaches the component or mutates an
+ * existing component.
+ *
+ * If attaching or mutating, this creates a copy of the
+ * given JSON object, owned by the given EC instance.
+ *
+ * @param ec - The EC instance to mutate.
+ * @param entity - the numeric ID of the entity to mutate.
+ * @param component - the name of the component to mutate.
+ * @param buffer - the string buffer containing raw JSON
+ * text.
+ * May be NULL (in which case tok must be also NULL) to
+ * detach.
+ * @param tok - the JSON top object to set as the component
+ * of the given entity.
+ * May be NULL (in which case buffer must also be NULL),
+ * or point to a JSON null object, to detach.
+ */
+void ec_set_component(struct ec *ec,
+		       u32 entity,
+		       const char *component,
+		       const char *buffer,
+		       const jsmntok_t *tok);
+
+/** ec_set_component_datum
+ *
+ * @brief Like ec_set_component, but accepts a null-terminated
+ * string containing valid JSON text.
+ */
+void ec_set_component_datum(struct ec *ec,
+			    u32 entity,
+			    const char *component,
+			    const char *valuez);
+
+/** ec_detach
+ *
+ * @brief A convenient shortcut macro to use ec_set_component
+ * to detach a component.
+ */
+#define ec_detach(ec, entity, component) \
+	ec_set_component((ec), (entity), (component), NULL, NULL)
+
+#endif /* LIGHTNING_PLUGINS_PAYZ_EC_H */
