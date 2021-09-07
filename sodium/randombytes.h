9ff1890f68caa99b257dbda2f7c308783dcfafad
ZmnSCPxj jxPCSnmZ <ZmnSCPxj@protonmail.com>
2021-09-07 15:30:29 +0800

    sodium/randombytes.h: Shim.

---
+++ sodium/randombytes.h
@@ -0,0 +1,10 @@
+#ifndef PAYZ_SODIUM_RANDOMBYTES_H
+#define PAYZ_SODIUM_RANDOMBYTES_H
+#include"config.h"
+
+/* Shim to get common/wireaddr.c compiling.
+ * common/wireaddr.c does not actually use any functions or
+ * definitions from this file.
+ */
+
+#endif /* PAYZ_SODIUM_RANDOMBYTES_H */
