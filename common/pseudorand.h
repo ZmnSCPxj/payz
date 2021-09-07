2762c321c621b4874e9802bd3799533700fd23a2
ZmnSCPxj jxPCSnmZ <ZmnSCPxj@protonmail.com>
2021-09-07 14:59:29 +0800

    common/pseudorand.h: Add shim.

---
+++ common/pseudorand.h
@@ -0,0 +1,13 @@
+#ifndef PAYZ_COMMON_PSEUDORAND_H
+#define PAYZ_COMMON_PSEUDORAND_H
+#include"config.h"
+#include<stdint.h>
+
+/* Shim to get common/channel_id.c compiling.
+ * The only thing that needs the pseudorand functions is
+ * temporary_channel_id, which a pay algorithm has no
+ * need of.
+ */
+#define pseudorand(x) ((uint64_t) 0)
+
+#endif /* PAYZ_COMMON_PSEUDORAND_H */
