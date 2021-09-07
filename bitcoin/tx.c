3edce1e6b3560e42958226bfe9eddc15205b9353
ZmnSCPxj jxPCSnmZ <ZmnSCPxj@protonmail.com>
2021-09-07 15:53:38 +0800

    bitcoin/tx.c: Add conversion of txid to/from hexstrings.

---
+++ bitcoin/tx.c
@@ -0,0 +1,29 @@
+#include"tx.h"
+#include<ccan/short_types/short_types.h>
+#include<ccan/str/hex/hex.h>
+
+static void reverse_bytes(u8 *arr, size_t len)
+{
+	unsigned int i;
+
+	for (i = 0; i < len / 2; i++) {
+		unsigned char tmp = arr[i];
+		arr[i] = arr[len - 1 - i];
+		arr[len - 1 - i] = tmp;
+	}
+}
+bool bitcoin_txid_from_hex(const char *hexstr, size_t hexstr_len,
+			   struct bitcoin_txid *txid)
+{
+	if (!hex_decode(hexstr, hexstr_len, txid, sizeof(*txid)))
+		return false;
+	reverse_bytes(txid->shad.sha.u.u8, sizeof(txid->shad.sha.u.u8));
+	return true;
+}
+bool bitcoin_txid_to_hex(const struct bitcoin_txid *txid,
+			 char *hexstr, size_t hexstr_len)
+{
+	struct sha256_double rev = txid->shad;
+	reverse_bytes(rev.sha.u.u8, sizeof(rev.sha.u.u8));
+	return hex_encode(&rev, sizeof(rev), hexstr, hexstr_len);
+}
--- bitcoin/tx.h
+++ bitcoin/tx.h
@@ -2,8 +2,10 @@
 #define PAYZ_BITCOIN_TX_H
 #include"config.h"
 #include<bitcoin/shadouble.h>
+#include<ccan/tal/tal.h>
 
-/* Shim to get common/channel_id.c compiling.
+/* Shim to get common/channel_id.c and common/json_helpers.c
+ * compiling.
  * channel_id really only needs the TXID below, and we
  * really only need channel_id to get common/json_helpers.c
  * compiling.
@@ -12,5 +14,18 @@
 struct bitcoin_txid {
 	struct sha256_double shad;
 };
+
+struct bitcoin_tx { };
+struct bitcoin_outpoint {
+	struct bitcoin_txid txid;
+	u32 n;
+};
+
+#define linearize_tx(ctx, tx) tal_arr((ctx), char*, 0)
+
+bool bitcoin_txid_from_hex(const char *hexstr, size_t hexstr_len,
+			   struct bitcoin_txid *txid);
+bool bitcoin_txid_to_hex(const struct bitcoin_txid *txid,
+			 char *hexstr, size_t hexstr_len);
 
 #endif /* PAYZ_BITCOIN_TX_H */
