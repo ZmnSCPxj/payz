48084d085284dca34e913e3f2057b70f6b221265
ZmnSCPxj jxPCSnmZ <ZmnSCPxj@protonmail.com>
2021-09-07 22:52:01 +0800

    wire/wire.c: Add towire_tu64 and fromwire_tu64.

--- wire/wire.c
+++ wire/wire.c
@@ -1,7 +1,9 @@
 #include"wire.h"
-#include<string.h>
+#include<assert.h>
+#include<ccan/build_assert/build_assert.h>
 #include<ccan/endian/endian.h>
 #include<ccan/mem/mem.h>
+#include<string.h>
 
 #ifndef SUPERVERBOSE
 #define SUPERVERBOSE(x)
@@ -41,6 +43,61 @@ void towire(u8 **pptr, const void *data, size_t len)
 	memcpy(*pptr + oldsize, memcheck(data, len), len);
 }
 
+static u64 fromwire_tlv_uint(const u8 **cursor, size_t *max, size_t maxlen)
+{
+	u8 bytes[8];
+	size_t length;
+	be64 val;
+
+	assert(maxlen <= sizeof(bytes));
+
+	/* BOLT #1:
+	 *
+	 * - if `length` is not exactly equal to that required for the
+	 *   known encoding for `type`:
+	 *	- MUST fail to parse the `tlv_stream`.
+	 */
+	length = *max;
+	if (length > maxlen) {
+		SUPERVERBOSE("greater than encoding length");
+		fromwire_fail(cursor, max);
+		return 0;
+	}
+
+	memset(bytes, 0, sizeof(bytes));
+	fromwire(cursor, max, bytes + sizeof(bytes) - length, length);
+
+	/* BOLT #1:
+	 * - if variable-length fields within the known encoding for `type` are
+	 *   not minimal:
+	 *    - MUST fail to parse the `tlv_stream`.
+	 */
+	if (length > 0 && bytes[sizeof(bytes) - length] == 0) {
+		SUPERVERBOSE("not minimal");
+		fromwire_fail(cursor, max);
+		return 0;
+	}
+	BUILD_ASSERT(sizeof(val) == sizeof(bytes));
+	memcpy(&val, bytes, sizeof(bytes));
+	return be64_to_cpu(val);
+}
+static void towire_tlv_uint(u8 **pptr, u64 v)
+{
+	u8 bytes[8];
+	size_t num_zeroes;
+	be64 val;
+
+	val = cpu_to_be64(v);
+	BUILD_ASSERT(sizeof(val) == sizeof(bytes));
+	memcpy(bytes, &val, sizeof(bytes));
+
+	for (num_zeroes = 0; num_zeroes < sizeof(bytes); num_zeroes++)
+		if (bytes[num_zeroes] != 0)
+			break;
+
+	towire(pptr, bytes + num_zeroes, sizeof(bytes) - num_zeroes);
+}
+
 int fromwire_peektype(const u8 *cursor)
 {
 	be16 be_type;
@@ -52,6 +109,10 @@ int fromwire_peektype(const u8 *cursor)
 	return be16_to_cpu(be_type);
 }
 
+u64 fromwire_tu64(const u8 **cursor, size_t *max)
+{
+	return fromwire_tlv_uint(cursor, max, 8);
+}
 u64 fromwire_u64(const u8 **cursor, size_t *max)
 {
 	be64 ret;
@@ -109,6 +170,10 @@ u8 *fromwire_tal_arrn(const tal_t *ctx,
 	return arr;
 }
 
+void towire_tu64(u8 **pptr, u64 v)
+{
+	return towire_tlv_uint(pptr, v);
+}
 void towire_u64(u8 **pptr, u64 v)
 {
 	be64 l = cpu_to_be64(v);
