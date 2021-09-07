9bc7843be7b849b1d2df02559d4052e9beee0764
ZmnSCPxj jxPCSnmZ <ZmnSCPxj@protonmail.com>
2021-09-07 15:28:41 +0800

    common/configdir.h: Shim.

---
+++ common/configdir.h
@@ -0,0 +1,10 @@
+#ifndef PAYZ_COMMON_CONFIGDIR_H
+#define PAYZ_COMMON_CONFIGDIR_H
+#include"config.h"
+
+/* Shim to get common/wireaddr.c compiling.
+ * common/wireaddr.c does not actually use any functions of
+ * this module.
+ */
+
+#endif /* PAYZ_COMMON_CONFIGDIR_H */
