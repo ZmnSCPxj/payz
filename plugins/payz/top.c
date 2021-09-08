7c4619f05a6c68a6e6e7abb3bacd920c800479bd
ZmnSCPxj jxPCSnmZ <ZmnSCPxj@protonmail.com>
2021-09-08 16:40:21 +0800

    plugins/payz/top.c: Construct ecs.

--- plugins/payz/top.c
+++ plugins/payz/top.c
@@ -1,6 +1,7 @@
 #include"top.h"
 #include<assert.h>
 #include<plugins/libplugin.h>
+#include<plugins/payz/ecs/ecs.h>
 
 struct payz_top *payz_top = NULL;
 
@@ -11,7 +12,7 @@ void setup_payz_top(const char *pay_command,
 	payz_top = tal(NULL, struct payz_top);
 
 	payz_top->disablempp = false;
-	payz_top->ecs = NULL; /* TODO.  */
+	payz_top->ecs = ecs_new(payz_top);
 	payz_top->commands = NULL; /* TODO.  */
 	payz_top->notifications = NULL; /* TODO.  */
 	payz_top->hooks = NULL; /* TODO.  */
