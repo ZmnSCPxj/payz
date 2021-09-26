610fa95ae60b84178c199edda40a888e8610e405
ZmnSCPxj jxPCSnmZ <ZmnSCPxj@protonmail.com>
2021-09-26 23:08:37 +0800

    plugins/payz/parsing.c: Factor out json_to_array_of_strings.

--- plugins/payz/parsing.c
+++ plugins/payz/parsing.c
@@ -1,12 +1,14 @@
 #include"parsing.h"
 #include"ccan/json_escape/json_escape.h"
 #include"common/json_command.h"
+#include"common/utils.h"
 
-struct command_result *
-param_array_of_strings(struct command *cmd, const char *name,
-		       const char *buffer, const jsmntok_t *tok,
-		       const char ***arr)
+const char **
+json_to_array_of_strings(const tal_t *ctx,
+			 const char *buffer, const jsmntok_t *tok)
 {
+	const char **arr;
+
 	struct json_escape *esc;
 	const jsmntok_t *j_str;
 	size_t i;
@@ -14,29 +16,40 @@ param_array_of_strings(struct command *cmd, const char *name,
 	/* As a convenience, if a single string, promote to a 1-entry
 	 * array of strings.  */
 	if (tok->type == JSMN_STRING) {
-		*arr = tal_arr(cmd, const char *, 1);
-		esc = json_escape_string_(cmd, buffer + tok->start,
+		arr = tal_arr(ctx, const char *, 1);
+		esc = json_escape_string_(tmpctx, buffer + tok->start,
 					  tok->end - tok->start);
-		(*arr)[0] = json_escape_unescape(cmd, esc);
-		if (!(*arr)[0])
-			goto fail;
-		return NULL;
+		arr[0] = json_escape_unescape(arr, esc);
+		if (!arr[0])
+			return tal_free(arr);
+		return arr;
 	}
 
 	if (tok->type != JSMN_ARRAY)
-		goto fail;
+		return NULL;
 
-	*arr = tal_arr(cmd, const char *, tok->size);
-	json_for_each_arr (i, j_str, tok) {
-		esc = json_escape_string_(cmd, buffer + j_str->start,
-					  j_str->end - j_str->start);
-		(*arr)[i] = json_escape_unescape(cmd, esc);
-		if (!(*arr)[i])
-			goto fail;
+	arr = tal_arr(ctx, const char *, tok->size);
+	json_for_each_arr(i, j_str, tok) {
+		if (j_str->type != JSMN_STRING)
+			return tal_free(arr);
+		esc = json_escape_string_(tmpctx, buffer + j_str->start,
+					   j_str->end - j_str->start);
+		arr[i] = json_escape_unescape(arr, esc);
+		if (!arr[i])
+			return tal_free(arr);
 	}
-	return NULL;
 
-fail:
+	return arr;
+}
+
+struct command_result *
+param_array_of_strings(struct command *cmd, const char *name,
+		       const char *buffer, const jsmntok_t *tok,
+		       const char ***arr)
+{
+	*arr = json_to_array_of_strings(cmd, buffer, tok);
+	if (*arr)
+		return NULL;
 	return command_fail_badparam(cmd, name, buffer, tok,
 				     "should be an array of strings.");
 }
--- plugins/payz/parsing.h
+++ plugins/payz/parsing.h
@@ -1,7 +1,8 @@
 #ifndef LIGHTNING_PLUGINS_PAYZ_PARSING_H
 #define LIGHTNING_PLUGINS_PAYZ_PARSING_H
 #include"config.h"
-#include"common/json.h"
+#include<common/json.h>
+#include<ccan/tal/tal.h>
 
 struct command;
 struct command_result;
@@ -10,5 +11,9 @@ struct command_result *
 param_array_of_strings(struct command *cmd, const char *name,
 		       const char *buffer, const jsmntok_t *tok,
 		       const char ***arr);
+
+const char **
+json_to_array_of_strings(const tal_t *ctx,
+			 const char *buffer, const jsmntok_t *tok);
 
 #endif /* LIGHTNING_PLUGINS_PAYZ_PARSING_H */
