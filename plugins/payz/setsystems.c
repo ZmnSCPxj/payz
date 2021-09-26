3293b7b611430d99c889a8c408d9ac8ae3701d20
ZmnSCPxj jxPCSnmZ <ZmnSCPxj@protonmail.com>
2021-09-26 22:28:13 +0800

    plugins/payz/setsystems.c: Factor out code to get and set `lightningd:systems`.

---
+++ plugins/payz/setsystems.c
@@ -0,0 +1,148 @@
+#include"setsystems.h"
+#include<ccan/json_out/json_out.h>
+#include<common/json_stream.h>
+#include<common/utils.h>
+#include<plugins/payz/ecs/ecs.h>
+#include<plugins/payz/top.h>
+
+static const char *syscomp = "lightningd:systems";
+
+void payz_generic_setsystems_tok(bool (*get_component)(const void *ec,
+						       const char **buffer,
+						       const jsmntok_t **toks,
+						       u32 entity,
+						       const char *component),
+				 void (*set_component)(void *ec,
+						       u32 entity,
+						       const char *component,
+						       const char *buffer,
+						       const jsmntok_t *tok),
+				 void *ec,
+				 u32 entity,
+				 const char *fieldname,
+				 const char *value_buffer,
+				 const jsmntok_t *value_toks)
+{
+	struct json_stream *js;
+
+	const char *buffer;
+	const jsmntok_t *toks;
+
+	size_t i;
+	const jsmntok_t *key;
+	const jsmntok_t *value;
+
+	const char *ncomp_buffer;
+	size_t ncomp_buffer_len;
+	jsmntok_t *ncomp_toks;
+
+	js = new_json_stream(tmpctx, NULL, NULL);
+	json_object_start(js, NULL);
+
+	/* Copy fields from existing object, if existing component is
+	 * an object.  */
+	(void) get_component(ec, &buffer, &toks, entity, syscomp);
+	if (toks->type == JSMN_OBJECT) {
+		json_for_each_obj (i, key, toks) {
+			/* Skip the field being modified.  */
+			if (json_tok_streq(buffer, key, fieldname))
+				continue;
+
+			value = key + 1;
+
+			json_add_tok(js, json_strdup(tmpctx, buffer, key),
+				     value, buffer);
+		}
+	}
+
+	/* Write the new field if not C NULL.  */
+	if (value_buffer)
+		json_add_tok(js, fieldname, value_toks, value_buffer);
+
+	json_object_end(js);
+
+	/* Extract the created object.  */
+	ncomp_buffer = json_out_contents(js->jout, &ncomp_buffer_len);
+	ncomp_toks = json_parse_simple(tmpctx,
+				       ncomp_buffer, ncomp_buffer_len);
+
+	/* Finally set the component.  */
+	set_component(ec, entity, syscomp, ncomp_buffer, ncomp_toks);
+}
+
+void payz_setsystems_tok(u32 entity,
+			 const char *fieldname,
+			 const char *buffer,
+			 const jsmntok_t *toks)
+{
+	typedef bool (*get_component_t)(const void *,
+					const char **,
+					const jsmntok_t **,
+					u32,
+					const char *);
+	typedef void (*set_component_t)(void *,
+					u32,
+					const char *,
+					const char *,
+					const jsmntok_t *);
+	payz_generic_setsystems_tok((get_component_t) &ecs_get_component,
+				    (set_component_t) &ecs_set_component,
+				    payz_top->ecs,
+				    entity, fieldname,
+				    buffer, toks);
+}
+
+void payz_setsystems(u32 entity,
+		     const char *fieldname,
+		     const char *value)
+{
+	jsmntok_t *toks;
+
+	toks = json_parse_simple(tmpctx, value, strlen(value));
+
+	payz_setsystems_tok(entity, fieldname, value, toks);
+}
+
+bool payz_generic_getsystems_(bool (*get_entity)(const void *ec,
+						 const char **buffer,
+						 const jsmntok_t **toks,
+						 u32 entity,
+						 const char *component),
+			      const void *ec,
+			      u32 entity,
+			      const char *fieldname,
+			      bool (*json_to_x)(const char *buffer,
+						const jsmntok_t *tok,
+						void *variable),
+			      void *variable)
+{
+	const char *buffer;
+	const jsmntok_t *toks;
+
+	if (!get_entity(ec, &buffer, &toks, entity, syscomp))
+		return false;
+
+	toks = json_get_member(buffer, toks, fieldname);
+	if (!toks)
+		return false;
+
+	return json_to_x(buffer, toks, variable);
+}
+
+bool payz_getsystems_(u32 entity,
+		      const char *fieldname,
+		      bool (*json_to_x)(const char *buffer,
+					const jsmntok_t *tok,
+					void *variable),
+		      void *variable)
+{
+	typedef bool (*get_component_t)(const void *,
+					const char **,
+					const jsmntok_t **,
+					u32,
+					const char *);
+	return payz_generic_getsystems_((get_component_t) &ecs_get_component,
+					payz_top->ecs,
+					entity, fieldname,
+					json_to_x, variable);
+}
---
+++ plugins/payz/setsystems.h
@@ -0,0 +1,157 @@
+#ifndef LIGHTNING_PLUGINS_PAYZ_SETSYSTEMS_H
+#define LIGHTNING_PLUGINS_PAYZ_SETSYSTEMS_H
+#include"config.h"
+#include<ccan/short_types/short_types.h>
+#include<ccan/typesafe_cb/typesafe_cb.h>
+#include<common/json.h>
+#include<stddef.h>
+
+/** payz_setsystems_tok
+ *
+ * @brief Set a field of the `lightningd:systems` component.
+ * 
+ * @desc By convention this component is an object.
+ * If not attached, or attached to a non-object, replaces
+ * it with an object, otherwise it creates a copy of the
+ * object with the specified field replaced, removed, or
+ * added.
+ *
+ * @param entity - the Entity to modify.
+ * @param fieldname - the field of `lightningd:systems` to
+ * modify, add, or remove.
+ * @param buffer - the buffer containing the JSON text.
+ * Can be NULL to remove the field instead.
+ * @param toks - an array of JSMN tokens of the parsed
+ * JSON text.
+ * If buffer is NULL, should be NULL as well
+ */
+void payz_setsystems_tok(u32 entity,
+			 const char *fieldname,
+			 const char *buffer,
+			 const jsmntok_t *toks);
+
+/** payz_setsystems
+ *
+ * @brief Set a field of the `lightningd:systems` component.
+ * 
+ * @desc By convention this component is an object.
+ * If not attached, or attached to a non-object, replaces
+ * it with an object, otherwise it creates a copy of the
+ * object with the specified field replaced, removed, or
+ * added.
+ *
+ * @param entity - the Entity to modify.
+ * @param fieldname - the field of `lightningd:systems` to
+ * modify, add, or remove.
+ * @param value - a nul-terminated C string containing valid
+ * JSON text.
+ */
+void payz_setsystems(u32 entity,
+		     const char *fieldname,
+		     const char *value);
+
+/** payz_generic_setsystems_tok
+ *
+ * @brief Like payz_setsystems_tok, but gets functions and object
+ * to use from the caller instead of from payz_top.
+ *
+ * @desc This is the real core function.
+ *
+ * @param get_component - function to get a component from
+ * the specified Entity-Component table.
+ * @param set_component - function to set a component in
+ * the specified Entity-Component table.
+ * @param ec - the Entity-Component table.
+ * @param entity - the Entity to modify.
+ * @param fieldname - the field of `lightningd:systems` to
+ * modify, add, or remove.
+ * @param buffer - the buffer containing the JSON text.
+ * Can be NULL to remove the field instead.
+ * @param toks - an array of JSMN tokens of the parsed
+ * JSON text.
+ * If buffer is NULL, should be NULL as well
+ */
+void payz_generic_setsystems_tok(bool (*get_component)(const void *ec,
+						       const char **buffer,
+						       const jsmntok_t **toks,
+						       u32 entity,
+						       const char *component),
+				 void (*set_component)(void *ec,
+						       u32 entity,
+						       const char *component,
+						       const char *buffer,
+						       const jsmntok_t *tok),
+				 void *ec,
+				 u32 entity,
+				 const char *fieldname,
+				 const char *buffer,
+				 const jsmntok_t *toks);
+
+/** payz_getsystems
+ *
+ * @brief Get a field of `lightningd:systems`, or return
+ * false on failure.
+ *
+ * @param entity - the Entity to query.
+ * @param fieldname - the field of `lightningd:systems`.
+ * @param json_to_x - the json_to_* function to use to
+ * parse the field.
+ * @param variable - the location to pass to the json_to_x
+ * function.
+ *
+ * @return - true if the field exists *and* parsing of
+ * json_to_x succeeded.
+ */
+bool payz_getsystems_(u32 entity,
+		      const char *fieldname,
+		      bool (*json_to_x)(const char *buffer,
+					const jsmntok_t *tok,
+					void *variable),
+		      void *variable);
+#define payz_getsystems(e, f, j, v) \
+	payz_getsystems_((e), (f), \
+			 typesafe_cb_preargs(bool, void*, \
+					     (j), (v), \
+					     const char *, \
+					     const jsmntok_t *), \
+			 (v))
+
+/** payz_generic_getsystems
+ *
+ * @brief like payz_getsystems, but uses the Entitcy Component
+ * table passed in.
+ *
+ * @param get_component - function to get the component from the
+ * given Entity Component table.
+ * @param ec - the Entity Component table.
+ * @param entity - the Entity to query.
+ * @param fieldname - the field of `lightningd:systems`.
+ * @param json_to_x - the json_to_* function to use to
+ * parse the field.
+ * @param variable - the location to pass to the json_to_x
+ * function.
+ *
+ * @return - true if the field exists *and* parsing of
+ * json_to_x succeeded.
+ */
+bool payz_generic_getsystems_(bool (*get_entity)(const void *ec,
+						 const char **buffer,
+						 const jsmntok_t **toks,
+						 u32 entity,
+						 const char *component),
+			      const void *ec,
+			      u32 entity,
+			      const char *fieldname,
+			      bool (*json_to_x)(const char *buffer,
+						const jsmntok_t *tok,
+						void *variable),
+			      void *variable);
+#define payz_generic_getsystems(ge, ec, e, f, j, v) \
+	payz_generic_getsystems_((ge), (ec), (e), (f), \
+				 typesafe_cb_preargs(bool, void*, \
+						     (j), (v), \
+						     const char *, \
+						     const jsmntok_t *), \
+				 (v))
+
+#endif /* LIGHTNING_PLUGINS_PAYZ_SETSYSTEMS_H */
