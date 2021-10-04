226e23992b3e98ca366672009aa521c8a7b6698d
ZmnSCPxj jxPCSnmZ <ZmnSCPxj@protonmail.com>
2021-10-04 15:28:10 +0800

    plugins/payz/systems/parse_invoice.c: System to parse the invoice.

---
+++ plugins/payz/systems/parse_invoice.c
@@ -0,0 +1,195 @@
+#include"parse_invoice.h"
+#include<ccan/tal/str/str.h>
+#include<common/errcode.h>
+#include<common/jsonrpc_errors.h>
+#include<common/json.h>
+#include<common/json_stream.h>
+#include<common/utils.h>
+#include<plugins/libplugin.h>
+
+static void parse_invoice(struct ecs *, struct command *,
+			  const char *buffer, const jsmntok_t *eo);
+static void promote_invoice_type(struct ecs *ecs, struct command *cmd,
+				 const char *buffer, const jsmntok_t *eo);
+
+struct ecs_register_desc system_parse_invoice[] = {
+	ECS_REGISTER_NAME("lightningd:parse_invoice"),
+	ECS_REGISTER_FUNC(&parse_invoice),
+	ECS_REGISTER_REQUIRE("lightningd:invoice"),
+	ECS_REGISTER_DISALLOW("lightningd:parse_invoice:ran"),
+	ECS_REGISTER_DONE(),
+
+	ECS_REGISTER_NAME("lightningd:promote_invoice_type"),
+	ECS_REGISTER_FUNC(&promote_invoice_type),
+	ECS_REGISTER_REQUIRE("lightningd:invoice:type"),
+	ECS_REGISTER_DISALLOW("lightningd:promote_invoice_type:ran"),
+	ECS_REGISTER_DONE(),
+
+	ECS_REGISTER_OVER_AND_OUT()
+};
+
+/*-----------------------------------------------------------------------------
+Invoice Parsing
+-----------------------------------------------------------------------------*/
+
+struct parse_invoice_closure {
+	struct ecs *ecs;
+	struct command *cmd;
+	u32 entity;
+};
+
+static struct command_result *
+parse_invoice_decode_ok(struct command *cmd,
+			 const char *buf,
+			 const jsmntok_t *res,
+			 struct parse_invoice_closure *closure);
+static struct command_result *
+parse_invoice_decode_ng(struct command *cmd,
+			 const char *buf,
+			 const jsmntok_t *res,
+			 struct parse_invoice_closure *closure);
+
+static void parse_invoice(struct ecs *ecs, struct command *cmd,
+			  const char *buffer, const jsmntok_t *eo)
+{
+	const char *error;
+
+	u32 entity;
+	const jsmntok_t *invoice;
+
+	struct parse_invoice_closure *closure;
+	struct out_req *req;
+
+	error = json_scan(tmpctx, buffer, eo,
+			  "{entity:%}",
+			  JSON_SCAN(json_to_u32, &entity));
+	if (error) {
+		plugin_log(cmd->plugin, LOG_UNUSUAL,
+			   "Expecting entity ID: %.*s",
+			   json_tok_full_len(eo), json_tok_full(buffer, eo));
+		return;
+	}
+
+	invoice = json_get_member(buffer, eo, "lightningd:invoice");
+	if (!invoice) {
+		plugin_log(cmd->plugin, LOG_UNUSUAL,
+			   "Expecting 'lightningd:invoice': %.*s",
+			   json_tok_full_len(eo), json_tok_full(buffer, eo));
+		return;
+	}
+
+	/* Prevent ourselves from running again.  */
+	ecs_set_component_datum(ecs, entity,
+				"lightningd:parse_invoice:ran", "true");
+
+	/* Keep our needed variables around.  */
+	closure = tal(cmd, struct parse_invoice_closure);
+	closure->ecs = ecs;
+	closure->cmd = cmd;
+	closure->entity = entity;
+
+	req = jsonrpc_request_start(cmd->plugin, cmd, "decode",
+				    &parse_invoice_decode_ok,
+				    &parse_invoice_decode_ng,
+				    closure);
+	json_add_tok(req->js, "string", invoice, buffer);
+	(void) send_outreq(cmd->plugin, req);
+}
+
+static struct command_result *
+parse_invoice_decode_ok(struct command *cmd,
+			 const char *buf,
+			 const jsmntok_t *res,
+			 struct parse_invoice_closure *closure)
+{
+	const jsmntok_t *key;
+	char *key_string;
+	const jsmntok_t *value;
+	size_t i;
+
+	tal_steal(tmpctx, closure);
+
+	json_for_each_obj(i, key, res) {
+		value = key + 1;
+
+		key_string = tal_fmt(tmpctx, "lightningd:invoice:%.*s",
+				     /* Get string contents and not include
+				      * the \" delimiters, so cannot use
+				      * json_tok_full.
+				      */
+				     key->end - key->start,
+				     buf + key->start);
+
+		ecs_set_component(closure->ecs, closure->entity,
+				  key_string,
+				  buf, value);
+	}
+
+	ecs_advance_done(cmd->plugin, closure->ecs, closure->entity);
+
+	return NULL;
+}
+static struct command_result *
+parse_invoice_decode_ng(struct command *cmd,
+			 const char *buf,
+			 const jsmntok_t *res,
+			 struct parse_invoice_closure *closure)
+{
+	tal_steal(tmpctx, closure);
+
+	ecs_set_component(closure->ecs, closure->entity,
+			  "lightningd:error", buf, res);
+	ecs_set_component(closure->ecs, closure->entity,
+			  "lightningd:systems", NULL, NULL);
+
+	return NULL;
+}
+
+/*-----------------------------------------------------------------------------
+Invoice Type Promotion
+-----------------------------------------------------------------------------*/
+
+/*~
+ * The `decode` command also returns a `type` field containing a
+ * string type for the input string.
+ * Promote it to a flag-type Component: for example if the type
+ * is `"bolt11 invoice"` attach a Component with the name
+ * `lightningd:invoice:type:bolt11 invoice`.
+ */
+
+static void promote_invoice_type(struct ecs *ecs, struct command *cmd,
+				 const char *buffer, const jsmntok_t *eo)
+{
+	u32 entity;
+
+	const char *error;
+
+	const jsmntok_t *type_tok;
+	char *type;
+
+	error = json_scan(tmpctx, buffer, eo,
+			  "{entity:%}",
+			  JSON_SCAN(json_to_u32, &entity));
+	if (error) {
+		plugin_log(cmd->plugin, LOG_UNUSUAL,
+			   "Expecting entity ID: %.*s",
+			   json_tok_full_len(eo), json_tok_full(buffer, eo));
+		return;
+	}
+	type_tok = json_get_member(buffer, eo, "lightningd:invoice:type");
+	if (!type_tok) {
+		plugin_log(cmd->plugin, LOG_UNUSUAL,
+			   "Expecting 'lightningd:invoice:type': %.*s",
+			   json_tok_full_len(eo), json_tok_full(buffer, eo));
+		return;
+	}
+
+	type = json_strdup(tmpctx, buffer, type_tok);
+
+	ecs_set_component_datum(ecs, entity,
+				tal_fmt(tmpctx, "lightningd:invoice:type:%s",
+					type),
+				"true");
+
+	ecs_advance_done(cmd->plugin, ecs, entity);
+}
---
+++ plugins/payz/systems/parse_invoice.h
@@ -0,0 +1,8 @@
+#ifndef LIGHTNING_PLUGINS_PAYZ_SYSTEMS_PARSE_INVOICE_H
+#define LIGHTNING_PLUGINS_PAYZ_SYSTEMS_PARSE_INVOICE_H
+#include"config.h"
+#include<plugins/payz/ecs/ecs.h>
+
+extern struct ecs_register_desc system_parse_invoice[];
+
+#endif /* LIGHTNING_PLUGINS_PAYZ_SYSTEMS_PARSE_INVOICE_H */
